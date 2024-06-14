4d355735031027f482b57448612f81654975a41f
Mridul <games.incr@outlook.com>
2024-06-14 19:47:54 +0530

    Multiple Class and Calling Methods

--- src/apples.java
+++ src/apples.java
@@ -1,28 +1,7 @@
-import java.util.Scanner;
+
 class apples{
     public static void main(String []args){
-        //get age to switch
-
-        Scanner INPage = new Scanner(System.in);
-
-        int age;
-
-        System.out.print("What is your age: ");
-        age = INPage.nextInt();
-        //switch code at age
-        switch (age){
-            case 1:
-                System.out.println("You are 1");
-                break;
-            case 2:
-                System.out.println("You can talk");
-                break;
-            case 3:
-                System.out.println("You Are 3");
-                break;
-            default:
-                System.out.print("It's okay");
-                break;
-        }
+        tuna tunaObject = new tuna();
+        tunaObject.simplemsg();
     }
 }
---
+++ src/tuna.java
@@ -0,0 +1,5 @@
+public class tuna {
+    public void simplemsg(){
+        System.out.println("Yet another JAVA Class");
+    }
+}
