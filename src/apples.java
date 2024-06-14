27a01782a8b2684af832b1ac4e0074d8f587b5c0
Mridul <games.incr@outlook.com>
2024-06-14 13:06:44 +0530

    print double as var

--- src/apples.java
+++ src/apples.java
@@ -1,5 +1,9 @@
 class apples{
     public static void main(String []args){
-        System.out.println("Hello YouTube!");
+
+        double tuna = 5.28;
+
+        System.out.println(tuna);
+
     }
 }
